202c4ff32686ac120f17bcdeccbd05f1d89d118b
Meiza Fleitas <meizafleitas@meizas-macbook-pro.local>
2016-01-03 18:46:19 -0500

    Meiza

--- iNUSurvey/ViewController.m
+++ iNUSurvey/ViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
